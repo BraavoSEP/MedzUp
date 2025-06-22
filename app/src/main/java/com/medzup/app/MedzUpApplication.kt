47dfb426db93ab5cac24d7863e96a48bb946d773
BraavoSEP <jonasdecarli@gmail.com>
2025-06-22 13:34:37 -0300

    Otimizando aplicação, para maior fluidez

--- app/src/main/java/com/medzup/app/MedzUpApplication.kt
+++ app/src/main/java/com/medzup/app/MedzUpApplication.kt
@@ -1,39 +1,20 @@
 package com.medzup.app
 
 import android.app.Application
-import android.content.res.Configuration
-import android.os.Build
 import com.medzup.app.managers.LanguageManager
 import dagger.hilt.android.HiltAndroidApp
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltAndroidApp
 class MedzUpApplication : Application() {
-    
+
     @Inject
     lateinit var languageManager: LanguageManager
 
     override fun onCreate() {
         super.onCreate()
-        
-        // Configurar idioma padrão como português
-        setDefaultLocale()
+        // O LanguageManager agora lida com a inicialização e aplicação
+        // do idioma correto (português como padrão ou o salvo pelo usuário).
         languageManager.initialize()
-    }
-    
-    private fun setDefaultLocale() {
-        val locale = Locale("pt")
-        Locale.setDefault(locale)
-        
-        val config = Configuration(resources.configuration)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            config.setLocale(locale)
-        } else {
-            @Suppress("DEPRECATION")
-            config.locale = locale
-        }
-        
-        resources.updateConfiguration(config, resources.displayMetrics)
     }
 } 
